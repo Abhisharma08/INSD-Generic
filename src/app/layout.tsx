996c00b60cfe3e47f02a138d1776ef91b6dbc56d
Abhimanyu Sharma <56254966+Abhisharma08@users.noreply.github.com>
2026-04-03 13:11:48 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Belleza&family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
         <Script
           async
-          src="https://www.googletagmanager.com/gtag/js?id=AW-18038800208"
+          src="https://www.googletagmanager.com/gtag/js?id=K6TRCZN5"
           strategy="afterInteractive"
         />
         <Script id="google-ads-tag" strategy="afterInteractive">
